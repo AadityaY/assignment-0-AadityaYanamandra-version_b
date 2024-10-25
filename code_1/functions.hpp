e2228482231db2b594afa1bb6ec26ce0afcdfcf7
AadityaY <67976359+AadityaY@users.noreply.github.com>
2024-10-25 16:25:01 -0600

    Add files via upload

---
+++ code_1/functions.hpp
@@ -0,0 +1,13 @@
+#ifndef READFILE_H__
+#define READFILE_H__
+
+#include <string>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// fooA returns -3.
+int fooA();
+
+#endif // READFILE_H__
